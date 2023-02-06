dbcc010c65626f8526ad1efd699df83b233b8ee3
rahulprajapatconsultadd <prajapat7401rahul@gmail.com>
2023-02-06 16:34:03 -0500

    code-clean

--- collegedata/studentdata/views.py
+++ collegedata/studentdata/views.py
@@ -13,14 +13,10 @@ from studentdata.serializers import DepartmentsSerializer, StudentDetailsSeriali
 @csrf_exempt
 @api_view(['GET','POST','DELETE','PUT'])
 def departmentApi(request, id=0):
-    print("$$$$$Inside DepartmentApi")
     if request.method == 'GET':
-        print("$$$$$Inside DepartmentApi GetRequest")
         try:
             departments_data = Departments.objects.all() 
-            # print("Department_data :", departments_data)
             department_serializer = DepartmentsSerializer(departments_data,many=True)
-            # print("Department_serializer :", department_serializer.data)
             return JsonResponse(department_serializer.data,safe=False)
         except Exception as e:
             df = {
@@ -34,17 +30,12 @@ def departmentApi(request, id=0):
     if request.method == 'POST':
         print("$$$$$Inside DepartmentApi POSTRequest")
         try:
-            # department_data = JSONParser().parse(request)
-            print("request_____data",request.data)
             department_serializer = DepartmentsSerializer(data=request.data)
-            print("department_serializer",department_serializer)
             if department_serializer.is_valid():
                 department_serializer.save()
-                print("reached")
                 return Response(department_serializer.data,status = status.HTTP_201_CREATED)
             return Response("Failed to Add the POST Request", safe=False)
         except Exception as e:
-            print("error---------")
             df = {
                 "Error_Message" : "Something went wrong in DepartmentAPI POST METHOD",
                 "Error" : e
